6882c823f625756e834f4527ea5fafcb805e2ebb
AqeilDanish <150001497+AqeilDanish@users.noreply.github.com>
2024-04-16 11:18:27 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -11,7 +11,7 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    int num = myObj.nextInt();
+    int num = in.nextInt();
 
     System.out.println("Number is: " + num * num);
   }
